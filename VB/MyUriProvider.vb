8b5694e42aac275185f7a35c37d581c38566eb1a
DevExpressExampleBot <fakeEmail>
2025-03-05 20:00:32 +0400

    autogenerate VB

--- VB/MyUriProvider.vb
+++ VB/MyUriProvider.vb
@@ -1,43 +1,40 @@
-Imports System.IO
+Imports System.Text
+Imports System.Drawing
+Imports System.Drawing.Imaging
 Imports DevExpress.Office.Services
 Imports DevExpress.Office.Utils
 Imports DevExpress.Drawing
 
 Namespace ExportOnlyBodyContent
+
     Public Class MyUriProvider
         Implements IUriProvider
 
-        Private rootDirectory As String
+        Private rootDirecory As String
 
-        Public Sub New(rootDirectory As String)
-            If String.IsNullOrEmpty(rootDirectory) Then
-                Exceptions.ThrowArgumentException("rootDirectory", rootDirectory)
-            End If
-            Me.rootDirectory = rootDirectory
+        Public Sub New(ByVal rootDirectory As String)
+            If [String].IsNullOrEmpty(rootDirectory) Then Exceptions.ThrowArgumentException("rootDirectory", rootDirectory)
+            rootDirecory = rootDirectory
         End Sub
 
-        Public Function CreateCssUri(rootUri As String, styleText As String, relativeUri As String) As String Implements IUriProvider.CreateCssUri
-            Dim cssDir As String = String.Format("{0}\{1}", Me.rootDirectory, rootUri.Trim("/"c))
-            If Not Directory.Exists(cssDir) Then
-                Directory.CreateDirectory(cssDir)
-            End If
-            Dim cssFileName As String = String.Format("{0}\style.css", cssDir)
+        Public Function CreateCssUri(ByVal rootUri As String, ByVal styleText As String, ByVal relativeUri As String) As String Implements IUriProvider.CreateCssUri
+            Dim cssDir As String = [String].Format("{0}\{1}", rootDirecory, rootUri.Trim("/"c))
+            If Not Directory.Exists(cssDir) Then Directory.CreateDirectory(cssDir)
+            Dim cssFileName As String = [String].Format("{0}\style.css", cssDir)
             File.AppendAllText(cssFileName, styleText)
-            Return GetRelativePath(cssFileName)
+            Return Me.GetRelativePath(cssFileName)
         End Function
 
-        Public Function CreateImageUri(rootUri As String, image As OfficeImage, relativeUri As String) As String Implements IUriProvider.CreateImageUri
-            Dim imagesDir As String = String.Format("{0}\{1}", Me.rootDirectory, rootUri.Trim("/"c))
-            If Not Directory.Exists(imagesDir) Then
-                Directory.CreateDirectory(imagesDir)
-            End If
-            Dim imageName As String = String.Format("{0}\{1}.png", imagesDir, Guid.NewGuid())
+        Public Function CreateImageUri(ByVal rootUri As String, ByVal image As OfficeImage, ByVal relativeUri As String) As String Implements IUriProvider.CreateImageUri
+            Dim imagesDir As String = [String].Format("{0}\{1}", rootDirecory, rootUri.Trim("/"c))
+            If Not Directory.Exists(imagesDir) Then Directory.CreateDirectory(imagesDir)
+            Dim imageName As String = [String].Format("{0}\{1}.png", imagesDir, Guid.NewGuid())
             image.DXImage.Save(imageName, DXImageFormat.Png)
-            Return GetRelativePath(imageName)
+            Return Me.GetRelativePath(imageName)
         End Function
 
-        Private Function GetRelativePath(path As String) As String
-            Dim substring As String = path.Substring(Me.rootDirectory.Length)
+        Private Function GetRelativePath(ByVal path As String) As String
+            Dim substring As String = path.Substring(rootDirecory.Length)
             Return substring.Replace("\", "/").Trim("/"c)
         End Function
     End Class
